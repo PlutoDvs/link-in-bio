85141ddb0ae71936eb81029f6aaa8a6b41ca6614
reza <87351115+M-reza-kd@users.noreply.github.com>
2025-01-12 22:41:53 +0330

    changing db to sqlite

--- urltree/urltree/settings.py
+++ urltree/urltree/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'urltree.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('POSTGRES_DB_NAME'),
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': env('POSTGRES_HOST'),
-        'PORT': env('POSTGRES_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
